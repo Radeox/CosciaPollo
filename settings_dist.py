b001734f68d5663c9e594e93fddd7103c0cb99fc
Radeox <dawid.weglarz95@gmail.com>
2017-09-05 04:12:20 +0200

    Added settings template

---
+++ settings_dist.py
@@ -0,0 +1,10 @@
+# Your Bot token (get it from BotFather)
+token = 'TOKEN'
+
+# List of words detected by the bot
+hot_words = 'pollo'
+
+# URL where the bot get the images
+search_keyword = 'chicken+leg'
+image_url = 'https://encrypted.google.com/search?q=' +\
+            search_keyword + '&tbm=isch'
